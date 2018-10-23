290a0e088dc0ee0f07f41e77d16dc4fc660fe2f4
TRIGianni <gianni.tricarico@heh.be>
2018-10-23 16:34:30 +0200

    Correction de l'injection de dépendance

--- petclinic-web/src/be/heh/petclinic/PetClinicApplication.java
+++ petclinic-web/src/be/heh/petclinic/PetClinicApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-//@ImportResource({"classpath*:be/heh/petclinic/config.xml","classpath*:be/heh/petclinic/**/component.xml"})
-@ImportResource("classpath*:be/heh/petclinic/config.xml")
+@ImportResource({"classpath*:be/heh/petclinic/config.xml","classpath*:be/heh/petclinic/**/component.xml"})
+//@ImportResource("classpath*:be/heh/petclinic/config.xml")
 
 public class PetClinicApplication {
 
