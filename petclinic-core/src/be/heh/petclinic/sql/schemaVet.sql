ac3521361aa04064fdb9b8ea1e05dab80771f694
Legacy <fabio.cumbo@std.heh.be>
2018-11-13 14:57:54 +0100

    Ajout des pets dans la DB

--- petclinic-core/src/be/heh/petclinic/sql/schemaVet.sql
+++ petclinic-core/src/be/heh/petclinic/sql/schemaVet.sql
@@ -6,3 +6,12 @@ CREATE TABLE vet (
     firstname varchar(45) not null,
     speciality char(45) not null,
     primary key (id) );
+
+DROP TABLE IF EXISTS pet;
+
+CREATE TABLE pet (
+    id int NOT NULL AUTO_INCREMENT,
+    type varchar(45) not null,
+    name varchar(45) not null,
+    birthdate DATE,
+    primary key (id) );
