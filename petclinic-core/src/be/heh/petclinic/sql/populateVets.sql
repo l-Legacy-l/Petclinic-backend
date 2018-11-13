e7143093614958041e83bdcb6f13ee871c8a3f6c
Alex <kourkhouleim@hotmail.fr>
2018-11-13 14:42:43 +0100

    DB

--- petclinic-core/src/be/heh/petclinic/sql/populateVets.sql
+++ petclinic-core/src/be/heh/petclinic/sql/populateVets.sql
@@ -3,3 +3,9 @@ INSERT INTO vet (lastname, firstname, speciality) VALUES ("James", "James", "non
 INSERT INTO vet (lastname, firstname, speciality) VALUES ("Helen", "Helen", "radiology");
 
 INSERT INTO vet (lastname, firstname, speciality) VALUES ("Linda", "Linda", "surgery");
+
+INSERT INTO owner (lastname, firstname, address, city, telephone) VALUES("Nick", "Nick", "49 Rue de la paix", "Paris", "0494012345");
+
+INSERT INTO owner (lastname, firstname, address, city, telephone) VALUES("Jane", "Jane", "50 Rue de la croix", "Paris", "0494123456");
+
+INSERT INTO owner (lastname, firstname, address, city, telephone) VALUES("Jim", "Jim", "51 Rue de la fosse", "Bruxelles", "0494234567");
--- petclinic-core/src/be/heh/petclinic/sql/schemaVet.sql
+++ petclinic-core/src/be/heh/petclinic/sql/schemaVet.sql
@@ -6,3 +6,14 @@ CREATE TABLE vet (
     firstname varchar(45) not null,
     speciality char(45) not null,
     primary key (id) );
+
+DROP TABLE IF EXISTS owner;
+
+CREATE TABLE owner (
+    id int NOT NULL AUTO_INCREMENT,
+    lastname varchar(45) not null,
+    firstname varchar(45) not null,
+    address char(45) not null,
+    city varchar(45) not null,
+    telephone varchar(45) not null,
+    primary key (id) );
