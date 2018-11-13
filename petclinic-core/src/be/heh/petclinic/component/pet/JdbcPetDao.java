3ba1da56b7ce19af38525638792db6238d041254
Legacy <fabio.cumbo@std.heh.be>
2018-11-13 15:19:06 +0100

    Implémentation des classes pour lire les données des animaux

---
+++ petclinic-core/src/be/heh/petclinic/component/pet/JdbcPetDao.java
@@ -0,0 +1,27 @@
+package be.heh.petclinic.component.vet;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+
+import be.heh.petclinic.domain.Vet;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+public class JdbcPetDao {
+    //Classe pour se connecter à la BDD et récupérer les données
+
+    private DataSource dataSource;
+
+    public JdbcPetDao(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
+    public List<Vet> getEvents() {
+        //JDBCTemplate Permet de faire la requete
+        JdbcTemplate select = new JdbcTemplate(dataSource);
+        return select.query("SELECT type, name, birthday, owner FROM pet", new VetRowMapper());
+    }
+
+}
+
---
+++ petclinic-core/src/be/heh/petclinic/component/pet/PetComponentImpl.java
@@ -0,0 +1,29 @@
+package be.heh.petclinic.component.vet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collection;
+
+import javax.sql.DataSource;
+
+import be.heh.petclinic.domain.*;
+
+class PetComponentImpl implements PetComponent {
+
+    private JdbcPetDao petDao;
+  
+    public PetComponentImpl(DataSource dataSource){
+        petDao = new JdbcpetDao(dataSource);
+    }
+
+    @Override
+    public Collection<Pet> getPets() {
+        List<Pet> pets = PetDao.getEvents();
+        //vets.add(new Vet("James","James","none"));
+        //vets.add(new Vet("Helen","Helen","radiology"));
+        //vets.add(new Vet("Linda","Linda","surgery"));
+        return pets;
+    }
+
+
+}
