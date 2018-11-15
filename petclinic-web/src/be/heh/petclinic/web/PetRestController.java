18e46f228abb2494b459bd634e7b21260132f931
Fabio Cumbo <legacy1810@gmail.com>
2018-11-15 16:10:44 +0100

    Correction du controlleur vet

--- petclinic-web/src/be/heh/petclinic/web/PetRestController.java
+++ petclinic-web/src/be/heh/petclinic/web/PetRestController.java
@@ -25,6 +25,9 @@ import java.util.List;
 import java.util.Collection;
 import java.util.Map;
 
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+
 @RestController
 
 public class PetRestController {
@@ -43,6 +46,22 @@ public class PetRestController {
 			return new ResponseEntity<Collection<Pet>>(HttpStatus.NOT_FOUND);
 		}
 		return new ResponseEntity<Collection<Pet>>(pets,HttpStatus.OK);
+	}
+
+	@RequestMapping(value="api/v1/petInsert",params = {"type","name","birthdate","ownerFirstname",
+			"ownerLastname"},method = GET)
+	@ResponseBody
+	public ResponseEntity<Pet> insertPet(@RequestParam Map<String,String> param)
+	{
+		String type = param.get("type");
+		String name = param.get("name");
+		String ownerFirstname = param.get("ownerFirstname");
+		String ownerLastname = param.get("ownerLastname");
+		String birthdate = param.get("birthdate");
+		System.out.println(birthdate);
+
+		PetComponentImpl.addPet(type,name,birthdate,ownerFirstname,ownerLastname);
+		return new ResponseEntity<Pet>(HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "api/v1/pets/tri",params = {"type"}, method = RequestMethod.GET)
